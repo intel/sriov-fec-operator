37149c8d0946e9eaa77531da8199723af796e929
ryan raasch <ryan.raasch@gmail.com>
2021-05-14 10:46:27 +0200

    Makefile structure cleanup. Need to come back to this.

--- scripts/install-tools.sh
+++ scripts/install-tools.sh
@@ -7,9 +7,9 @@ BIN=$(realpath $BASE/../bin)
 DOWNLOADS=$(realpath $BASE/../downloads)
 REQUIRED_OPERATOR_SDK_VERSION="${1:-v1.4.2}"
 SDK_URL="https://github.com/operator-framework/operator-sdk/releases/download"
-OPM_URL="https://mirror.openshift.com/pub/openshift-v4/x86_64/clients/ocp/latest-4.6"
-OPM_FILE="opm-linux-4.6.26.tar.gz"
-OC_URL="https://mirror.openshift.com/pub/openshift-v4/x86_64/clients/ocp/4.6.23"
+OPM_URL="https://mirror.openshift.com/pub/openshift-v4/x86_64/clients/ocp/stable-4.6"
+OPM_FILE="opm-linux-4.6.27.tar.gz"
+OC_URL="https://mirror.openshift.com/pub/openshift-v4/x86_64/clients/ocp/stable-4.6"
 OC_FILE="openshift-client-linux.tar.gz"
 
 if [ ! -e $BIN ] ; then
