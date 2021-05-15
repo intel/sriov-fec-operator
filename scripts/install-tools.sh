ea5b169a64582460d5a7d111f12f849727816601
Ryan Raasch <rmr@silicom.dk>
2021-05-15 11:25:50 +0200

    Install kustomize and controller-gen.

--- scripts/install-tools.sh
+++ scripts/install-tools.sh
@@ -8,7 +8,7 @@ DOWNLOADS=$(realpath $BASE/../downloads)
 REQUIRED_OPERATOR_SDK_VERSION="${1:-v1.4.2}"
 SDK_URL="https://github.com/operator-framework/operator-sdk/releases/download"
 OPM_URL="https://mirror.openshift.com/pub/openshift-v4/x86_64/clients/ocp/stable-4.6"
-OPM_FILE="opm-linux-4.6.27.tar.gz"
+OPM_FILE="opm-linux-4.6.28.tar.gz"
 OC_URL="https://mirror.openshift.com/pub/openshift-v4/x86_64/clients/ocp/stable-4.6"
 OC_FILE="openshift-client-linux.tar.gz"
 
@@ -26,11 +26,14 @@ if [ ! -e $BIN/operator-sdk ] ; then
 fi
 
 if [ ! -e $BIN/opm ] ; then
-    curl -sL $OPM_URL/$OPM_FILE -o $DOWNLOADS/$OPM_FILE
-    tar xvf $DOWNLOADS/$OPM_FILE -C $BIN
+    curl -sL $OPM_URL/$OPM_FILE -o $DOWNLOADS/opm.tar.gz
+    tar xvf $DOWNLOADS/opm.tar.gz -C $BIN
 fi
 
 if [ ! -e $BIN/oc ] ; then
     curl -sL $OC_URL/$OC_FILE -o $DOWNLOADS/$OC_FILE
     tar xvf $DOWNLOADS/$OC_FILE -C $BIN
 fi
+
+GOBIN=$BIN go get sigs.k8s.io/controller-tools/cmd/controller-gen
+GOBIN=$BIN go get sigs.k8s.io/kustomize/kustomize
