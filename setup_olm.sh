51a4576858b76aaa7f22819b8617dfc5e333ecfe
Ryan Raasch <rmr@silicom.dk>
2021-05-21 11:51:58 +0200

    Added README material.

--- setup_olm.sh
+++ setup_olm.sh
@@ -6,7 +6,7 @@ set +x
 
 oc delete ns intel-fpga-operators
 
-oc delete Operatorgroups operator-sdk-og -n default
+# oc describe csv n3000.v2.0.0 -n intel-fpga-operators
 # oc delete csv n3000.v1.1.0 -n default
 # oc delete Catalogsources n3000-catalog -n default
 # oc delete Subscription n3000-v2-0-0-sub -n default
@@ -15,3 +15,7 @@ oc delete Operatorgroups operator-sdk-og -n default
 oc create ns intel-fpga-operators
 #oc new-project intel-fpga-operators
 operator-sdk run bundle quay.io/ryan_raasch/intel-fpga-bundle:v2.0.0 --verbose -n intel-fpga-operators
+
+# oc describe csv n3000.v2.0.0 -n intel-fpga-operator
+# oc describe Clusterserviceversion -n intel-fpga-operators
+# oc describe Operatorgroups operator-sdk-og -n intel-fpga-operators
