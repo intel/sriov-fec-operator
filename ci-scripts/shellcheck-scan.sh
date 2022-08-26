5d9d86004408380133cab145d585a02e47037944
Tadeusz Mateńko <tadeusz.matenko@intel.com>
2022-08-26 13:28:56 +0200

    Run shellcheck against selected files (#69)

--- ci-scripts/shellcheck-scan.sh
+++ ci-scripts/shellcheck-scan.sh
@@ -5,18 +5,21 @@ function run_chell_check()
     local OPERATOR_HOME="$1"
     shellcheck -V
     cd ${OPERATOR_HOME}
-    find . -name '*.sh' ! -path "./go*" -print -exec sha256sum {} ';' | tee shellcheck-scan.txt
-    echo "========================================" >> shellcheck-scan.txt
-    find . -type f -name "*.sh" ! -path "./.git/*"  ! -path './go*' ! -path "./copyright.sh" ! -path "./ci-scripts/*" -print -exec shellcheck {} ';' | tee -a shellcheck-scan.txt
-    if [ -n "$(find . -type f -name '*.sh' ! -path "./.git/*" ! -path "./copyright.sh" ! -path "./go/*" ! -path "./ci-scripts/*" | head -1)" ]; then
-        find . -name "*.sh" | xargs shellcheck; EXITCODE=$?
+    find . -name '*.sh' -print -exec sha256sum {} ';' | tee shellcheck-scan.txt
+    echo "========================================" | tee -a shellcheck-scan.txt
+    scripts=$(find . -type f -name "*.sh" ! -path "./.git/*" ! -path "./copyright.sh" ! -path "./ci-scripts/*")
+    if [ -n "$scripts" ]; then
+        echo $'Files to scan:\n'"$scripts" | tee -a shellcheck-scan.txt
+        echo "========================================" | tee -a shellcheck-scan.txt
+        shellcheck $scripts | tee -a shellcheck-scan.txt
+        EXITCODE=${PIPESTATUS[0]}
         if [ $EXITCODE -eq 0 ]; then
-            echo "xargs exit code: " $EXITCODE - "No issues found" >> shellcheck-scan.txt
+            echo "shellcheck exit code: $EXITCODE - No issues found" | tee -a shellcheck-scan.txt
         else
-            echo "xargs exit code: " $EXITCODE - "Issues found and logged to shellcheck-scan.txt"
+            echo "shellcheck exit code: $EXITCODE - Issues found and logged to shellcheck-scan.txt"
         fi
     else
-        echo "No *.sh files found"
+        echo "No *.sh files found to scan"
     fi
     exit $EXITCODE
 }
